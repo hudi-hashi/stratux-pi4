6b68571588c2a74ae5534a85f1997f8ad69bc374
Hudi <132931651+hudi-hashi@users.noreply.github.com>
2025-03-26 14:39:36 -0700

    Update setup-pi4-standard.sh

--- setup-pi4-standard.sh
+++ setup-pi4-standard.sh
@@ -86,7 +86,7 @@ cd /root
 rm -rf /root/stratux
 git clone --recursive https://github.com/stratux/stratux.git /root/stratux
 cd /root/stratux
-git checkout 5283a06 # last verified version
+git checkout ff1f01d # last verified version
 
 # copy various files from /root/stratux/image
 cd /root/stratux/image
