cdbca479238a3d7314217b698fabe30ea88437d6
Hudi <132931651+hudi-hashi@users.noreply.github.com>
2026-06-01 11:50:16 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "ASIAXAUV5ITFVV23EEZO"
+  access_key = "ASIAXAUV5ITFVK83EEZO"
   secret_key = "qlkqXSWZU5IglHw1m1Jgl+7GmMiimIbIw5Omoydb"
   token = ""
   region = "us-west-1"
